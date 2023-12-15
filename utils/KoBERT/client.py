e55844ef8eff3e3c0d0fbdbd4e76424d9167b2b8
rnjsdydgus0 <rnjsdydgus0@gmail.com>
2023-12-15 17:40:04 +0900

    231215 - 2 번역 기능 추가. 임시 streamlit run app.py 실행후 page1 에서 테스트 맨처음 오류나는건 input_text 박스 초기값이 빈 문자열이라서 번역기능에 오류가 생겨서 그런것. 나중에 챗봇으로 넘어가면 발생되지 않을것으로 예상.

--- utils/KoBERT/client.py
+++ utils/KoBERT/client.py
@@ -1,32 +1,57 @@
-from module import find_similarity_top_3, kobert, find_near_place
+from utils.KoBERT.module import find_similarity_top_3, kobert, find_near_place
+from googletrans import Translator
 import pandas as pd
 import os
 import pickle
 
-# 유사도 top 3
+
 file_path = os.path.dirname(__file__)
 parent_path = os.path.dirname(file_path)
+
+#여행지 데이터 불러오기
 place_df = pd.read_csv(parent_path + '\\data\\place.csv')
 
-with open(parent_path + '\\data\\kobert_embeddings.pkl', 'rb') as file:
-    base_embeddings = pickle.load(file)
-
-# 정보받기
-user_data = input('가고 싶은 곳에 대해 설명 해 주세요\n')
-target_embedding = kobert(user_data)
-
-
-top_3_df = find_similarity_top_3(place_df, base_embeddings, target_embedding)
-
-#유사도가 가장 높은 장소와 가장 가까운 숙소 3곳
+#숙소 데이터 불러오기
 hotel_df = pd.read_csv(parent_path + '\\data\\hotel.csv')
 
-near_hotels = find_near_place(hotel_df, top_3_df.loc[0])
+#임베딩된 문장들 불러오기
+with open(parent_path + '\\data\\kobert_embeddings.pkl', 'rb') as file:
+    base_embeddings = pickle.load(file)
 
 # 장소 , 링크 데이터 불러오기
 with open(parent_path + '\\data\\name_link_dict.pkl', 'rb') as file:
     name_link_dict = pickle.load(file)
 
+def app(user_text):
+    translator = Translator()
+    # 언어 감지 후 번역
+    trans_user_text_data = translator.translate(user_text, dest='ko')
+    trans_user_text = trans_user_text_data.text
+    trans_from_lang = trans_user_text_data.src
+    trans_to_lang = trans_user_text_data.dest # maybe, KOREAN
+    
+    target_embedding = kobert(trans_user_text)
+
+    #유사도 높은 장소 3곳
+    top_3_df = find_similarity_top_3(place_df, base_embeddings, target_embedding)
+
+    #유사도가 가장 높은 장소와 가장 가까운 숙소 3곳
+    near_hotels = find_near_place(hotel_df, top_3_df.loc[0])
+
+    # 대상언어로 번역
+    for idx, name in enumerate(top_3_df.name):
+        top_3_df.name[idx] = translator.translate(name, src=trans_to_lang, dest=trans_from_lang).text
+    for idx, info in enumerate(top_3_df['info']):
+        top_3_df['info'][idx] = translator.translate(info, src=trans_to_lang, dest=trans_from_lang).text
+    for idx, name in enumerate(near_hotels.name):
+        near_hotels.name[idx] = translator.translate(name, src=trans_to_lang, dest=trans_from_lang).text
+    for idx, info in enumerate(near_hotels['info']):
+        near_hotels['info'][idx] = translator.translate(info, src=trans_to_lang, dest=trans_from_lang).text
+    #유사도가 가장 높은 장소와 가장 가까운 음식점 3곳
+    #near_foods = 
+    return top_3_df, near_hotels
+
+'''
 print(f'추천 여행지 \n{top_3_df.name[0]}')
 print(name_link_dict[top_3_df.name[0]])
 print(f'이곳도 가보는건 어때요?\n {top_3_df.name[1]} \n {top_3_df.name[2]}')
@@ -38,3 +63,4 @@ print(f'{near_hotels.name[1]}')
 print(f'{name_link_dict[near_hotels.name[1]]}')
 print(f'{near_hotels.name[2]}')
 print(f'{name_link_dict[near_hotels.name[2]]}')
+'''
--- utils/project1_desc.py
+++ utils/project1_desc.py
@@ -1,7 +1,11 @@
 import streamlit as st
+from utils.KoBERT import client
+import pickle
+
+
 
 def desc():
-	st.write('''
-
-
-	''')
+    user_input_data = st.text_input('Please describe where you would like to travel.')
+    top_3_df, near_hotels = client.app(user_input_data)
+    st.write(f'첫번째 추천 : {top_3_df.name[0]}')
+    st.info(f"{top_3_df['info'][0]}")
