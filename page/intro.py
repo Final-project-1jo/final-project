c48c633f631d4960397671af98ff66fb3fcb2a3e
HansolKim <onepinetreek@gmail.com>
2023-12-26 09:59:35 +0900

    챗봇 업데이트

--- page/intro.py
+++ page/intro.py
@@ -1,12 +1,14 @@
 import streamlit as st
 from streamlit_chat import message
-
+from utils import project1_desc as p1d
 import hashlib
 import hmac
 import base64
 import time
 import requests
 import json
+import re
+from utils.Word2Vec import client, module
 class ChatbotMessageSender:
 
     # chatbot api gateway url
@@ -71,7 +73,7 @@ class ChatbotMessageSender:
 
 
 if __name__ == '__main__':
-    input_text = '주요대사'
+    input_text = '안녕'
     res_obj = ChatbotMessageSender()
     res = res_obj.req_message_send(input_text)
 
@@ -106,20 +108,52 @@ def desc():
     with st.form('form', clear_on_submit=True): # 채팅 입력창 생성
         user_input = st.text_input('User: ', '') # 입력부분
         submitted = st.form_submit_button('Send') # 전송 버튼
-    
+ 
     
     if submitted and user_input:
         user_input1 = user_input.strip() # 채팅 입력값 및 여백제거
-        res = ChatbotMessageSender().req_message_send(user_input1)
-        chatbot_output1 = res.json()['bubbles'][0]['data']['description']
-        st.session_state.past.append(user_input1) # 입력값을 past 에 append -> 채팅 로그값 저장을 위해
-        st.session_state.generated.append(chatbot_output1)
+        if not re.match(r'^태그', user_input1):  # ^태그로 시작하지 않는 경우
+            res = ChatbotMessageSender().req_message_send(user_input1)
+            chatbot_output1 = res.json()['bubbles'][0]['data']['description']
+            st.session_state.past.append(user_input1) # 입력값을 past 에 append -> 채팅 로그값 저장을 위해
+            st.session_state.generated.append(chatbot_output1)
+            
+        elif re.match(r'^태그', user_input1):
+            korean_to_target_list, target_to_korean_dict = module.show_tag('ko')
+            # "태그" 다음의 문자열을 쉼표로 분할하여 리스트로 만듭니다.
+            tag_removed = user_input1.split('태그 ')[1].split(',')
+            s_boxes = [tag.strip() for tag in tag_removed]
+            #s_boxes = re.findall(r'(?!태그)\b\w+\b', user_input1)
+            print(s_boxes)
+            # res = ChatbotMessageSender().req_message_send('선택태그')
+            # chatbot_output1 = res.json()['bubbles'][0]['data']['description']
+            # st.session_state.past.append(user_input1)
+            # st.session_state.generated.append(s_boxes) 
+            
+        # else:
+            st.session_state.past.append(user_input1)
+            
+            user_data = [tag for tag in s_boxes if not 'X' in tag]
+            place_df = client.app0(user_data)
+            near_hotel_df, near_food_df = client.app1(place_df.iloc[0])
+            
+            rec_string = '선택한 태그에 가장 적합한 목적지입니다.\n['+place_df.name[0] + '](' + place_df.link[0]+')\n\n다음은 목적지 주변 호텔입니다.\n['+near_hotel_df.name[0] + '](' +near_hotel_df.link[0]+') , ['+near_hotel_df.name[1] + '](' +near_hotel_df.link[1]+') , ['+near_hotel_df.name[2] + '](' +near_hotel_df.link[2]+')\n\n다음은 목적지 주변 맛집입니다.\n['+near_food_df.name[0] + '](' +near_food_df.link[0]+') , ['+near_food_df.name[1] + '](' +near_food_df.link[1]+') , ['+near_food_df.name[2] + '](' +near_food_df.link[2]+')\n'
+            #rec_string = 'This is the best destination for the tag you selected\n['+place_df.name[0] + '](' + place_df.link[0]+')\n\nthe followings are hotels close to the '+place_df.name[0] + '\n['+near_hotel_df.name[0] + '](' +near_hotel_df.link[0]+') , ['+near_hotel_df.name[1] + '](' +near_hotel_df.link[1]+') , ['+near_hotel_df.name[2] + '](' +near_hotel_df.link[2]+')\n\nthe followings are some of the best places to eat near the '+place_df.name[0] + '\n['+near_food_df.name[0] + '](' +near_food_df.link[0]+') , ['+near_food_df.name[1] + '](' +near_food_df.link[1]+') , ['+near_food_df.name[2] + '](' +near_food_df.link[2]+')\n'
+
+            res = ChatbotMessageSender().req_message_send('태그 여기')
+            chatbot_output1 = res.json()['bubbles'][0]['data']['description']
+            st.session_state.generated.append(rec_string +'\n'+ chatbot_output1)
+
     
     with placeholder.container(): # 리스트에 append된 채팅입력과 로봇출력을 리스트에서 꺼내서 메세지로 출력
         for i in range(len(st.session_state['past'])):
             message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+
             if len(st.session_state['generated']) > i:
                 message(st.session_state['generated'][i], key=str(i) + '_bot')
+                
+  
+
 
 def app():
     desc()
